d30cbcaa7f320912a7ad4aaff83da2604050a535
zixuan.tian <zixuan.tianzx@nio.com>
2018-11-21 21:03:20 +0800

    二叉树中序遍历

---
+++ src/main/java/net/tianzx/leetcode/BinaryTree.java
@@ -0,0 +1,41 @@
+package net.tianzx.leetcode;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: tianzx
+ * Date: 2018/11/20  6:20 PM
+ * Email: zixuan.tian@nio.com
+ */
+public class BinaryTree {
+    //input [1,null,2,3]
+    //output [1,3,2] inorder travle
+    private static class TreeNode {
+        public Integer val;
+        public TreeNode left;
+        public TreeNode right;
+
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .append("value", val)
+                    .append("leftNode", left)
+                    .append("rightNode", right)
+                    .toString();
+        }
+    }
+
+    List<Integer> list = new ArrayList();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            list.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return list;
+    }
+}
