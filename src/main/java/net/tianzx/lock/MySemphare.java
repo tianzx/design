c28773f804d661c1ffec619935695935c2eede42
tianzx <436011080@qq.com>
2018-12-02 20:41:37 +0800

    Semphare init

---
+++ src/main/java/net/tianzx/lock/MySemphare.java
@@ -0,0 +1,4 @@
+package net.tianzx.lock;
+
+public class MySemphare {
+}
--- src/test/java/net/tianzx/Test.java
+++ src/test/java/net/tianzx/Test.java
@@ -1,13 +1,45 @@
 package net.tianzx;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
 /**
  * Author: tianzx
  * Date: 2018-11-28  11:53
  * Email: zixuan.tian@nio.com
  */
 public class Test {
+    Semaphore semaphore = new Semaphore(2);
 
-    public static void main(String[] args) {
-        System.err.println(Integer.valueOf(""));
+    ExecutorService service = Executors.newFixedThreadPool(10);
+
+
+    static class MyTask implements  Runnable {
+        Semaphore semaphore;
+
+        public MyTask(Semaphore semaphore) {
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            System.err.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            semaphore.release();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Test test = new Test();
+        MyTask myTask = new MyTask( test.semaphore);
+        for(int i=0;i<4;i++) {
+            test.semaphore.acquire(1);
+            test.service.submit(myTask);
+        }
+        test.service.shutdown();
     }
 }
