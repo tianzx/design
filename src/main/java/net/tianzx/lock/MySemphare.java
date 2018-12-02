f8bff45f3108e07263438c11d81907801c19c39f
tianzx <436011080@qq.com>
2018-12-02 23:17:44 +0800

    Semphare init

--- src/main/java/net/tianzx/lock/MySemphare.java
+++ src/main/java/net/tianzx/lock/MySemphare.java
@@ -1,4 +1,47 @@
 package net.tianzx.lock;
 
-public class MySemphare {
+
+import java.io.Serializable;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class MySemphare implements Serializable {
+
+    private final Sync sync;
+
+    public MySemphare(int permits) {
+        sync = new MySemphare.NonfairSync(permits);
+    }
+
+    abstract static class Sync extends AbstractQueuedSynchronizer {
+        Sync(int permits) {
+            setState(permits);
+        }
+
+        protected int nonfairTryAcquireShared(int acquires) {
+            return 0;
+        }
+
+    }
+
+    static final class NonfairSync extends MySemphare.Sync {
+        private static final long serialVersionUID = -2694183684443567898L;
+
+        NonfairSync(int permits) {
+            super(permits);
+        }
+
+        protected int tryAcquireShared(int acquires) {
+            return nonfairTryAcquireShared(acquires);
+        }
+    }
+
+    public void acquire() throws InterruptedException {
+        sync.acquireSharedInterruptibly(1);
+    }
+
+    public void release(int permits) {
+        if (permits < 0) throw new IllegalArgumentException();
+        sync.releaseShared(permits);
+    }
+
 }
