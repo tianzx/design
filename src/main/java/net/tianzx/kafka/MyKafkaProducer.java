1c1e0450b94efad766916b660b96c4d7d85f71c4
tianzx <tianzx@aliyun.com>
2018-12-12 12:33:56 +0800

    test while() producer

--- src/main/java/net/tianzx/kafka/MyKafkaProducer.java
+++ src/main/java/net/tianzx/kafka/MyKafkaProducer.java
@@ -12,8 +12,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Author: tianzx
@@ -29,14 +29,14 @@ public class MyKafkaProducer {
         props.put("bootstrap.servers", "10.125.243.61:9092,10.125.242.97:9092,10.125.252.75:9092");
         props.put("acks", "1");
         props.put("buffer.memory", 33_554_432);
-        props.put("batch.size", 1_048_576);
-        props.put("max.request.size", 10_485_760);
+        props.put("batch.size", 16_384);
+//        props.put("max.request.size", 10_485_760);
         props.put("client.id", "msg_api_" + 1);
-        props.put("retries", 30);
+        props.put("retries", 3);
         props.put("linger.ms", 1);
 //        props.put("retry.backoff.ms", 3000);
-//        props.put("max.block.ms", 30000);
-//        props.put("request.timeout.ms", 10);
+//        props.put("max.block.ms", 60000);
+        props.put("request.timeout.ms", 30000);
         props.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
         props.put("security.protocol", "SASL_PLAINTEXT");
@@ -56,48 +56,36 @@ public class MyKafkaProducer {
         myKafkaProducer.init(props);
         Producer<String, String> producer = new KafkaProducer(props);
         String topicName = "swc-cvs-nmp-mac-10001-notification";
-        String value = "test";
-        int msgNum = 10000;
-        String nonce = "";
+        byte[] value = Utils.readFile("test.file");
+        int msgNum = 2_00;
         List list = Lists.newArrayList();
-        for (int i = 0; i < msgNum; i++) {
-            long startTime = System.currentTimeMillis();
-
-            ProducerRecord pr = new ProducerRecord(topicName, null, startTime, nonce.getBytes(), value.getBytes());
-            LOGGER.info("record producer send time {}", startTime);
-            Future<RecordMetadata> future = producer.send(pr, (recordMetadata, e) -> {
-                LOGGER.info(String.valueOf(recordMetadata.partition()));
-                long midTime = recordMetadata.timestamp();
-                long endTime = System.currentTimeMillis();
-//                LOGGER.info("record kafka arrive time {}", midTime);
-                LOGGER.info("record receive time {}", endTime - startTime);
-                if (e != null) {
-                    LOGGER.debug("send2kafka: fail to send msgSize {} of topic {} to kafka {}, ",
-                            recordMetadata.serializedValueSize(), recordMetadata.topic(), e);
-                } else {
-                    LOGGER.info("fail" + e.getMessage());
-                    LOGGER.debug("send2kafka: send msg {} of clientId {} to kafka {} success");
-                }
-            });
-            list.add(future);
-        }
-        list.forEach(result -> {
-            Future<RecordMetadata> future = (Future<RecordMetadata>) result;
-            try {
-//                System.err.println(future.get());
-                LOGGER.info("send success :{}", ((Future<RecordMetadata>) result).get());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        });
+        long start = System.currentTimeMillis();
+        AtomicLong al = new AtomicLong(0);
+        int num = 0;
+        long startTime = System.currentTimeMillis();
         while (true) {
-            long startTime = System.currentTimeMillis();
-            Thread.sleep(1000);
-            long endTime = System.currentTimeMillis();
-            LOGGER.info(String.valueOf(endTime - startTime));
-
+            for (int i = 0; i < msgNum; i++) {
+                String nonce = i + "";
+                ProducerRecord pr = new ProducerRecord(topicName, null, startTime, nonce.getBytes(), value);
+                Future<RecordMetadata> future = producer.send(pr, (recordMetadata, e) -> {
+                    LOGGER.info(String.valueOf(al.incrementAndGet()));
+                    if (e != null) {
+                        LOGGER.debug("send2kafka: fail to send msgSize {} of topic {} to kafka {}, ",
+                                recordMetadata.serializedValueSize(), recordMetadata.topic(), e);
+                    } else {
+//                        producer.send(pr, (recordMetadata2, e2) -> {
+//                            LOGGER.info(String.valueOf(al.getAndAdd(1)));
+//                            if (e2 != null) {
+//                                LOGGER.debug("send2kafka: fail to send msgSize {} of topic {} to kafka {}, ",
+//                                        recordMetadata.serializedValueSize(), recordMetadata.topic(), e);
+//                            } else {
+//                                LOGGER.info("send2kafka: success");
+//                            }
+//                        });
+                    }
+                });
+            }
         }
+
     }
 }
